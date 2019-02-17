8108c8d1979ba7195afb3ffc0fc0e30b5acec72f
Eric Leask <16efl@queensu.ca>
2019-02-17 11:35:58 -0500

    Bug Fix: Return to Standby from Run
    System only would only check the button if the fc was off. Fixed.

--- FC_Controller/FC_Controller.ino
+++ FC_Controller/FC_Controller.ino
@@ -153,15 +153,14 @@ void System() {
   // wait for input to set fc_on to true
 
   if (!fc_on)
-    //setColorState(LED_ON, 0, LED_ON); // Sets state LED to magenta.
-
-    if (digitalRead(SYSTEM_ON_PIN) == HIGH) {
-      fc_on = !fc_on;
-      setColorState(LED_ON, LED_ON, LED_ON);
-      delay(1000); // to prevent us from flipping back and forth between on and off
-      //TODO: counter or delay? delay may cause a delay in control before switching off which may not be ideal.
-      // then again, turning the FC off kills the controller so I suppose it really doesn't matter
-    }
+    setColorState(LED_ON, 0, LED_ON); //Red and Blue
+  if (digitalRead(SYSTEM_ON_PIN) == HIGH) {
+    fc_on = !fc_on;
+    setColorState(LED_ON, LED_ON, LED_ON);
+    delay(1000); // to prevent us from flipping back and forth between on and off
+    //TODO: counter or delay? delay may cause a delay in control before switching off which may not be ideal.
+    // then again, turning the FC off kills the controller so I suppose it really doesn't matter
+  }
 
 
 }
@@ -223,7 +222,6 @@ void FC() {
     case (FC_RUN):
       // ?? manual info is copied from FC_STANDBY
       setColorState(0, LED_ON, LED_ON); //BLUE and YELLOW led
-
       setSupplyState(OPEN); // The supply value should always be open.
       setRelayState(CLOSED);
       AutomaticPurgeControl();
